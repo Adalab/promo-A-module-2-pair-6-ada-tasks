66e18c633f9fba789b46d460deb480843e8d6c5e
belenviton <belenviton@hotmail.com>
2023-12-16 11:54:37 +0100

    add througt

--- Js/main.js
+++ Js/main.js
@@ -1,5 +1,7 @@
 'use strict'
 
+//Query Selector
+
 
 // Crear array de tareas
 const tasks = [
@@ -20,11 +22,26 @@ const tasksList = document.querySelector('.js__tasks_list');
 // Función para pintar la tarea
 
 function renderTask(task){
-    tasksList.innerHTML += `<li><input type="checkbox">${task.name}</li>`;
+    tasksList.innerHTML += `<li class= "js__one_task js__checkbox"><input class= "" type="checkbox">${task.name}</li>`;
 }
+
 
 for ( const task of tasks ) {
     //codigo que queremos que se repita
     renderTask(task);
 }
 
+const oneTask = document.querySelectorAll('.js__one_task');
+const checkbox = document.querySelectorAll('.js__checkbox');
+
+
+// también se puede hacer cambiando el tasks.completed a True o False después de clickar, volver a pintar la lista HTML y despues añadir la clase tachar
+
+const handleClickCheckbox = (event) => {
+const clickedCheckbox = event.currentTarget;
+clickedCheckbox.classList.toggle ('through');
+  
+}
+
+for (const clickCheckbox of checkbox) { 
+clickCheckbox.addEventListener ('click' , handleClickCheckbox);};
